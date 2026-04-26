5645c6800aaf7ea83a4473c4fc040460cf3d232e
NicolasSKing <nocoben10@gmail.com>
2026-04-26 12:15:52 +0800

    here_i_created_the_class_then_made_the_first_def_function_this_function_is_for_all_the_names_of_txt_files_that_later_will_be_used

---
+++ square_of_even_and_cube_of_odd_integers.py
@@ -0,0 +1,7 @@
+class IntegerFileProcessor:
+    def __init__(self, input_file, even_output_file, odd_output_file):
+        self.input_file = input_file
+        self.even_output_file = even_output_file
+        self.odd_output_file = odd_output_file
+
+    
