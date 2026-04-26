c399ec1050ecd9e12786d06676847793cf00191a
NicolasSKing <nocoben10@gmail.com>
2026-04-26 12:47:40 +0800

    i_just_changed_the_single_quotes_in_r_and_w_to_double_quotes_and_the_variable_in_the_for_loop_the_one_that_turns_the_numbers_into_whole_num_i_changed_it_from_numbers_to_num

--- square_of_even_and_cube_of_odd_integers.py
+++ square_of_even_and_cube_of_odd_integers.py
@@ -5,14 +5,14 @@ class IntegerFileProcessor:
         self.odd_output_file = odd_output_file
 
     def process_files(self):
-        with open(self.input_file, 'r') as file:
+        with open(self.input_file, "r") as file:
             numbers = file.read().split()
 
-        with open(self.even_output_file, 'w') as even_file, open(self.odd_output_file, 'w') as odd_file:
+        with open(self.even_output_file, "w") as even_file, open(self.odd_output_file, "w") as odd_file:
             for num in numbers:
-                number = int(num)
+                num = int(num)
 
-                if number % 2 == 0:
+                if num % 2 == 0:
                     even_file.write(str(num ** 2) + '\n')
                 else:
                     odd_file.write(str(num ** 3) + '\n')
